f2046d185b81d7db964d182062ce364d163a898d
sri-go <36702009+sri-go@users.noreply.github.com>
2020-03-27 21:41:55 -0400

    Update Data per 27-Mar-2020, Filter Controls Finished

--- Data/data.js
+++ Data/data.js
@@ -1,73 +1,75 @@
-var url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-25-2020.csv';
+var get_data = function (url) {
+    var download = $.ajax({
+        url: url
+    });
+    var combined_promise = download.done(function (downloaded_data) {
+        if (typeof downloaded_data !== 'undefined') {
+            csv2geojson.csv2geojson(downloaded_data, function (err, data) {
+                // console.log(data);
+                var us_subsect = filter_us_data(data);
+                var calc_total = calc_totals(us_subsect);
+                var combined_data = combine_files(calc_total);
+                // console.log(combined_data);
+            });
+        };
+        // console.log(statesData);
+        return statesData;        
+        // console.log('Converted Data to GEOJSON');
+    });
 
-var download = $.ajax({
-    url: url
-});
-download.done(function (downloaded_data) {
-    if (typeof downloaded_data !== 'undefined') {
-       csv2geojson.csv2geojson(downloaded_data, function (err, data) {
-            // console.log(data);
-            var us_subsect = filter_us_data(data);
-            var calc_total = calc_totals(us_subsect);
-            var combined_data = combine_files(calc_total);
-            // console.log(combined_data);
-            return combined_data;
+    var filter_us_data = function (data) {
+        var us_subsect = [];
+        // filter out for only US Data
+        _.filter(data.features, function (feature) {
+            // console.log(feature);
+            var not_in = ['Grand Princess', 'Diamond Princess', 'Wuhan Evacuee', 'Recovered'];
+            // if(feature.properties.)  
+            if (not_in.indexOf(feature.properties['Province_State']) >= 0) {
+                delete feature;
+            } else if (feature.properties['Country_Region'] == 'US') {
+                us_subsect.push(feature);
+            }
         });
+        // console.log(us_subsect);
+        return us_subsect;
     };
-    // console.log('Converted Data to GEOJSON');
-});
 
-var filter_us_data = function (data) {
-    var us_subsect = [];
-    // filter out for only US Data
-    _.filter(data.features, function (feature) {
-        // console.log(feature);
-        var not_in = ['Grand Princess', 'Diamond Princess', 'Wuhan Evacuee', 'Recovered'];
-        // if(feature.properties.)  
-        if (not_in.indexOf(feature.properties['Province_State']) >= 0) {
-            delete feature;
-        } else if (feature.properties['Country_Region'] == 'US') {
-            us_subsect.push(feature);
-        }
-    });
-    // console.log(us_subsect);
-    return us_subsect;
+    var calc_totals = function (us_subsect_data) {
+        //calculate totals for each state;
+        var confirmed_state_total = _.reduce(us_subsect_data, function (memo, item) {
+            memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Confirmed);
+            return memo;
+        }, {});
+        // console.log(confirmed_state_total);
+        var death_state_total = _.reduce(us_subsect_data, function (memo, item) {
+            memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Deaths);
+            return memo;
+        }, {});
+        // console.log(death_state_total);
+        var recovered_state_total = _.reduce(us_subsect_data, function (memo, item) {
+            memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Recovered);
+            return memo;
+        }, {});
+        // console.log(recovered_state_total);
+        return {
+            confirmed_state_total: confirmed_state_total,
+            death_state_total: death_state_total,
+            recovered_state_total: recovered_state_total
+        };
+    }
+
+    var combine_files = function (data_totals) {
+        //add COVID-19 data to the states.js file
+        // console.log(Object.keys(data_totals));
+        var combined = _.each(statesData.features, function (state_feature) {
+            var state = state_feature.properties.name;
+            state_feature.properties.Confirmed = data_totals['confirmed_state_total'][state];
+            state_feature.properties.Deaths = data_totals['death_state_total'][state];
+            state_feature.properties.Recovered = data_totals['recovered_state_total'][state];
+            delete state_feature.properties.density;
+        });
+        //  console.log(statesData.features);
+        return combined;
+    };
+    return combined_promise;
 };
-
-var calc_totals = function (us_subsect_data) {
-    //calculate totals for each state;
-    var confirmed_state_total = _.reduce(us_subsect_data, function (memo, item) {
-        memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Confirmed);
-        return memo;
-    }, {});
-    // console.log(confirmed_state_total);
-    var death_state_total = _.reduce(us_subsect_data, function (memo, item) {
-        memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Deaths);
-        return memo;
-    }, {});
-    // console.log(death_state_total);
-    var recovered_state_total = _.reduce(us_subsect_data, function (memo, item) {
-        memo[item.properties.Province_State] = (memo[item.properties.Province_State] || 0) + parseInt(item.properties.Recovered);
-        return memo;
-    }, {});
-    // console.log(recovered_state_total);
-    return {
-        confirmed_state_total: confirmed_state_total,
-        death_state_total: death_state_total,
-        recovered_state_total: recovered_state_total
-    };
-}
-
-var combine_files = function (data_totals) {
-    //add COVID-19 data to the states.js file
-    // console.log(Object.keys(data_totals));
-    var combined = _.each(statesData.features, function (state_feature) {
-        var state = state_feature.properties.name;
-        state_feature.properties.Confirmed = data_totals['confirmed_state_total'][state];
-        state_feature.properties.Deaths = data_totals['death_state_total'][state];
-        state_feature.properties.Recovered = data_totals['recovered_state_total'][state];
-        delete state_feature.properties.density;
-    });
-    //  console.log(statesData.features);
-    return combined;
-}
--- js/main.js
+++ js/main.js
@@ -1,45 +1,53 @@
-  //Initalize Leaflet Map
-  var map = L.map('map', {
-    center: [37.8, -96],
-    zoom: 5
-  }); 
-
-  //Define what Map Tile Style to use
-  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
-    minZoom: 0,
-    maxZoom: 30,
-    ext: 'png'
-  }).addTo(map);
-
-  var featureGroup; 
+  var url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-27-2020.csv';
+  var featureGroup;
   
   //Determine Color Scheme
   function getColor(d) {
     return d > 100 ? '#084594' :
-           d > 50  ? '#2171b5' :
-           d > 30  ? '#4292c6' :
-           d > 20  ? '#6baed6' :
-           d > 10   ? '#9ecae1' :
-           d > 5   ? '#c6dbef' :
-           d > 1   ? '#deebf7' :
-                      '#f7fbff';
+      d > 50 ? '#2171b5' :
+      d > 30 ? '#4292c6' :
+      d > 20 ? '#6baed6' :
+      d > 10 ? '#9ecae1' :
+      d > 5 ? '#c6dbef' :
+      d > 1 ? '#deebf7' :
+      '#f7fbff';
   }
 
   //Logic for how the color is applied...by default it is applied to the Death count by state
-  var base_map_style = function(feature, count='Deaths'){
+  var style = 'Deaths';
+  var mortality_style = function (feature) {
     // console.log(feature);
-    // console.log(change)
-    return {
-      fillColor: getColor(feature.properties[count]),
+      return {
+      fillColor: getColor(feature.properties[style]),
       weight: 1,
       opacity: 1,
       color: 'white',
       dashArray: '2',
       fillOpacity: 0.7
     };
-  }
+  };
+
+  var confirmed_style = function(feature){
+    return {
+      fillColor: getColor(feature.properties.Confirmed),
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      dashArray: '2',
+      fillOpacity: 0.7
+    };
+  };
+
+  var recovered_style = function(feature){
+    return {
+      fillColor: getColor(feature.properties.Recovered),
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      dashArray: '2',
+      fillOpacity: 0.7
+    };
+  };
 
   //On Highlight or Mouse Hover What Happens
   function highlightFeature(e) {
@@ -72,37 +80,13 @@
 
   title.onAdd = function(map){
     var div = L.DomUtil.create('div', 'title');
-    div.innerHTML = '<h1>US Coronavirus Situation Map</h1> <h3><h3>Updated as of 26-Mar-2020</h3>';
+    div.innerHTML = '<h1>US Coronavirus Situation Map</h1> <h3><h3>Updated as of 27-Mar-2020</h3>';
     return div;
-     
+
   };
 
   title.addTo(map);
 
-  var filter = L.control({position: 'bottomleft'});
-  filter.onAdd = function(map){
-    this._div = L.DomUtil.create('div', 'filter');
-    this.select();
-    return this._div;
-  };
-
-  filter.select = function(){
-    this._div.innerHTML = '<h1>Filter Controls</h1>'
-    + '<input type="checkbox" id="cbox-input1" checked>' + '<label id="checkbox-label1" for="cbox-input1">Confirmed</label>'
-    + '<input type="checkbox" id="cbox-input2">' + '<label id="checkbox-label2" for="cbox-input2">Dead</label>'
-    + '<input type="checkbox" id="cbox-input3">' + '<label id="checkbox-label3" for="cbox-input3">Recovered</label>';
-
-  };
-
-  function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('check')
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    });
-  };
-
-  filter.addTo(map); 
-  
   var info = L.control();
 
   info.onAdd = function (map) {
@@ -122,19 +106,68 @@
 
   var legend = L.control({position: 'bottomright'});
   legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'info legend'),
-          levels = [0, 1, 5, 10, 20, 30, 50, 100];
-      div.innerHTML = ('<h4>US Coronavirus Death Count</h4>');
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < levels.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + getColor(levels[i] + 1) + '"></i> ' +
-              levels[i] + (levels[i + 1] ? '&ndash;' + levels[i + 1] + '<br>' : '+');
-      }
-      return div;
+    this._div = L.DomUtil.create('div', 'info legend'),
+    levels = [0, 1, 5, 10, 20, 30, 50, 100];
+    this._div.innerHTML = ('<h4>US Coronavirus Death Count</h4>');
+    for (var i = 0; i < levels.length; i++) {
+      this._div.innerHTML +=
+          '<i style="background:' + getColor(levels[i] + 1) + '"></i> ' +
+          levels[i] + (levels[i + 1] ? '&ndash;' + levels[i + 1] + '<br>' : '+');
+  }      
+    return this._div;
+  };
+
+  legend.update = function(str){
+     this._div.innerHTML = ('<h4>US Coronavirus '+str+' Count</h4>');
+     for (var i = 0; i < levels.length; i++) {
+      this._div.innerHTML +=
+          '<i style="background:' + getColor(levels[i] + 1) + '"></i> ' +
+          levels[i] + (levels[i + 1] ? '&ndash;' + levels[i + 1] + '<br>' : '+');
+  }      
   };
 
   legend.addTo(map);
+
+  var filter = L.control({position: 'bottomleft'});
+
+  filter.onAdd = function(map){
+    this._div = L.DomUtil.create('div', 'filter');
+    this.select();
+    return this._div;
+  };
+
+  filter.select = function(){
+    this._div.innerHTML = '<h1>Filter Controls</h1>'
+    + '<input name="check" type="checkbox" id="cbox-input1" onclick="filter_data(this)">' + '<label id="checkbox-label1" for="cbox-input1">Confirmed</label>'
+    + '<input name="check" type="checkbox" id="cbox-input2" onclick="filter_data(this)" checked>' + '<label id="checkbox-label2" for="cbox-input2">Dead</label>'
+    + '<input name="check" type="checkbox" id="cbox-input3" onclick="filter_data(this)">' + '<label id="checkbox-label3" for="cbox-input3">Recovered</label>';
+  };
+
+  function filter_data(checkbox) {
+    var checkboxes = document.getElementsByName('check');
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false;
+    });
+    if (checkbox.id == 'cbox-input1'){
+      style = 'Confirmed';
+      featureGroup.setStyle(confirmed_style);
+      legend.update('Confirmed');
+    }
+    if (checkbox.id == 'cbox-input2'){
+      style = 'Deaths';
+      featureGroup.setStyle(mortality_style);
+      legend.update('Death');
+
+    }
+    if (checkbox.id == 'cbox-input3'){
+      style = 'Recovered';
+      featureGroup.setStyle(recovered_style);
+      legend.update('Recovered');
+
+    }
+  };
+
+  filter.addTo(map); 
 
   var eachFeatureFunction = function(feature, layer) {
     layer.on({
@@ -145,18 +178,12 @@
   };
 
   $(document).ready(function() {
-    console.log(statesData);
-    featureGroup = L.geoJson(statesData, {
-      style: base_map_style,
-      onEachFeature: eachFeatureFunction
-    }).addTo(map);
+    get_data(url).done(function(){
+      featureGroup = L.geoJson(statesData, {
+        style: mortality_style,
+        onEachFeature: eachFeatureFunction
+      }).addTo(map);
+    });
+    // console.log(combined_data);
     $('.leaflet-control-zoom').hide();
-        
-    
-    $('input[type="checkbox"]').on('change', function() {
-      $('input[type="checkbox"]').not(this).prop('checked', false);
-   });
   });
-
-
-  
